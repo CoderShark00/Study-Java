51c5b4f8cd865312b6ef498e4059e923874b6a8c
hyuk2 <hyuk2000s@gmail.com>
2024-12-03 21:27:33 +0900

    first commit

---
+++ untitled/src/main/java/c09_classes/Student02.java
@@ -0,0 +1,16 @@
+package c09_classes;
+
+public class Student02 {
+    int studentCode;
+    int name;
+    int score;
+
+    public Student02(int studentCode, int name, int score) {
+        this.studentCode = studentCode;
+        this.name = name;
+        this.score = score;
+    }
+
+    public Student02() {
+    }
+}
